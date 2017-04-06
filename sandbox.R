ea293c03af9c5f0da2dda5a1982bef1bf2929c23
Unknown <scott@stoltzmanconsulting.com>
2017-04-06 11:39:53 -0600

    some data manipulation

--- sandbox.R
+++ sandbox.R
@@ -1,3 +1,36 @@
 library(rjson)
-json_file <- "http://api.worldbank.org/country?per_page=10&region=OED&lendingtype=LNX&format=json"
-json_data <- fromJSON(paste(readLines(json_file), collapse=""))
+library(dplyr)
+library(googleVis)
+
+creds = fromJSON(file = "credentials.json")
+
+endpoint = paste("https://data.colorado.gov/resource/idu3-cuww.json?$$app_token=",creds$token,sep="")
+
+cols = "*"
+rows = 2000
+offsetRows = 50
+selections = paste("&$select=",cols,sep="")
+
+
+
+request = paste(endpoint,query,sep="")
+
+data = fromJSON(getURL(request))
+
+df = data %>%
+  select(mailzipcode,
+         city,
+         state,
+         licensetype,
+         licenseexpirationdate,
+         licensestatusdescription,
+         licensefirstissuedate,
+         licenselastreneweddate,
+         degrees,
+         firstname,
+         lastname)
+
+library(lubridate)
+df$yearsToExpiration = round(as.numeric(as.Date(df$licenseexpirationdate) - today())/365,1)
+a = df[is.na(df$licenseexpirationdate) == FALSE,]
+b = a %>% filter(yearsToExpiration>0)
